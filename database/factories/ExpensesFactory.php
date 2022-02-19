1fade5e21862ffa0f981aba464a3ef955df077ef
linuxfabi <fabienne.fedier@gmail.com>
2022-02-19 23:12:32 +0100

    add food and expanse factory; move farmer migration some lower to prevent name violation

--- database/factories/ExpensesFactory.php
+++ database/factories/ExpensesFactory.php
@@ -7,6 +7,8 @@ use Faker\Generator as Faker;
 
 $factory->define(Expenses::class, function (Faker $faker) {
     return [
-        //
+        "reason" => $faker->realText(17),
+        "amount" => $faker->randomNumber(),
+        "admin_id" => Admin::firstOrFail()
     ];
 });
--- database/factories/FoodFactory.php
+++ database/factories/FoodFactory.php
@@ -3,10 +3,12 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\Models\Food;
+use App\Models\Admin;
 use Faker\Generator as Faker;
 
 $factory->define(Food::class, function (Faker $faker) {
     return [
-        //
+        "food_name" => $faker->unique()->name(),
+        "admin_id" => Admin::firstOrFail()
     ];
 });
